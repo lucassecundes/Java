64dee613de1c3ec9217e6827d064d3c417ff3b4b
aluno <aluno@mscgresl00416.CGR.ACAD>
2017-05-09 21:16:34 -0300

    Exemplo Aula10

---
+++ JavaAv2/src/Br/Estacio/Aula10/TesteDaClasseArrays.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Br.Estacio.Aula10;
+
+/**
+ *
+ * @author aluno
+ */
+public class TesteDaClasseArrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Declaração do objeto
+       
+       Arrays objetoDaClasseArrays;
+       // Instanciação
+       objetoDaClasseArrays = new Arrays();   
+       
+         boolean retorno;
+
+        for (int i = 0; i < 15; i++) {
+            retorno = objetoDaClasseArrays.insereDadosNoVetor(i, 2);
+            if (retorno) {
+                System.out.println("O valor " + 2 + " foi inserido na posicao " + i);
+            } else {
+                System.out.println("O valor " + 2 + " não foi inserido na posicao " + i);
+            }
+
+        }
+        objetoDaClasseArrays.insereDadosNoVetor(0, 1);
+        objetoDaClasseArrays.insereDadosNoVetor(1, 1);
+        objetoDaClasseArrays.insereDadosNoVetor(2, 1);
+        objetoDaClasseArrays.insereDadosNoVetor(3, 1);
+        objetoDaClasseArrays.insereDadosNoVetor(4, 1);
+        objetoDaClasseArrays.insereDadosNoVetor(5, 1);
+        objetoDaClasseArrays.insereDadosNoVetor(6, 1);
+        objetoDaClasseArrays.insereDadosNoVetor(7, 1);
+        objetoDaClasseArrays.insereDadosNoVetor(8, 1);
+        objetoDaClasseArrays.insereDadosNoVetor(9, 1);
+        objetoDaClasseArrays.insereDadosNoVetor(10, 1);
+        
+        int resultadoDaSoma;
+        
+        resultadoDaSoma = objetoDaClasseArrays.soma();
+        
+        System.out.println(
+                "O valor da soma eh: " + resultadoDaSoma);
+    
+    }
+    
+}
