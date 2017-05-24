22b0791a5cc47f05782cd6805e92dfa1e9ebe8af
aluno <aluno@mscgresl00416.CGR.ACAD>
2017-05-23 20:00:42 -0400

    Classe Pai e filhos

---
+++ JavaAv2/src/Br/Estacio/lucas/programacao/aula11/TestePessoa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Br.Estacio.lucas.programacao.aula11;
+
+/**
+ *
+ * @author aluno
+ */
+public class TestePessoa {
+    public static void main(String[] args[]){
+        pessoa pessoa= new Pessoa();
+        pessoa.dataNascimento="2017-01-01";
+        pessoa.nome="João";
+        pessoa.endereco="Rua da Saudade, nº328";
+    }
+}
